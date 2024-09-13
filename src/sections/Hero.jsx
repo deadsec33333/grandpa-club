e7a0d4f1d858c86818778c6a017e1f7411331b34
Ro kas <meskauskas80@gmail.com>
2024-09-13 23:09:00 +0300

    ajfs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
       {/* <h2 className="flex justify-center font-comic  text-[22px] mb-10 mt-0 text-[#dfe2ff]">F1 MEMES</h2> */}
       <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#000] animate-slide-in-left">CA: XXX  </h1>
 
-      <a href="https://pump.fun/2EwSdTsQVVjQusuKDPaVcAmVEJLWmbUEkMbty1a4pump"><button className="animate-bounce mt-6 bg-[#52d591] text-[#fff] rounded-full px-28 py-6 hover:bg-[#f0ff00] border-4 border-black  font-comic  text-[24px] ">
+      <a href="https://pump.fun/board"><button className="animate-bounce mt-6 bg-[#52d591] text-[#fff] rounded-full px-28 py-6 hover:bg-[#f0ff00] border-4 border-black  font-comic  text-[24px] ">
         BUY $POPS
       </button></a>
 
--- src/sections/Links.jsx
+++ src/sections/Links.jsx
@@ -3,10 +3,10 @@ import { findTitle, img1, img2, cardImage1, cardImage2, bottomLinks } from '../a
 
 const Links = () => {
   const cards = [
-    { title: "PUMPFUN", text: "BUY NOW", link: "https://pump.fun/2EwSdTsQVVjQusuKDPaVcAmVEJLWmbUEkMbty1a4pump", image: img2 },
+    { title: "PUMPFUN", text: "BUY NOW", link: "https://pump.fun/board", image: img2 },
     { title: "TELEGRAM", text: "Join Now", link: "https://t.me/GrandpasClub", image: cardImage1 },
     { title: "X", text: "Join Now", link: "https://x.com/GrandpasSolClub", image: img1 },
-    { title: "DEXSCREENER", text: "BUY NOW", link: "https://pump.fun/2EwSdTsQVVjQusuKDPaVcAmVEJLWmbUEkMbty1a4pump", image: cardImage2 }
+    { title: "DEXSCREENER", text: "BUY NOW", link: "https://pump.fun/board", image: cardImage2 }
   ];
 
   return (
