01d4795251081df49eb043e00aa12af1459dffa4
Ro kas <meskauskas80@gmail.com>
2024-09-17 20:55:58 +0300

    ajfs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,9 +3,9 @@ const Hero = () => {
     <section id="home" className="w-full h-screen flex flex-col justify-center items-center min-h-screen pb-[32rem] bg-cover bg-center">
       {/* <h1 className="flex justify-center font-comic  text-[70px] mb-0 text-[#fff]">$F1</h1> */}
       {/* <h2 className="flex justify-center font-comic  text-[22px] mb-10 mt-0 text-[#dfe2ff]">F1 MEMES</h2> */}
-      <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#000] animate-slide-in-left">CA: XXX  </h1>
+      <h1 className="flex justify-center font-comic  text-[35px] mt-6 text-center px-4 break-words text-[#000] animate-slide-in-left">CA: DWvGbXULoN3LwwGFvGswot6bq3UTvhuDMwrXWu1Dpump  </h1>
 
-      <a href="https://pump.fun/board"><button className="animate-bounce mt-6 bg-[#52d591] text-[#fff] rounded-full px-28 py-6 hover:bg-[#f0ff00] border-4 border-black  font-comic  text-[24px] ">
+      <a href="https://pump.fun/DWvGbXULoN3LwwGFvGswot6bq3UTvhuDMwrXWu1Dpump"><button className="animate-bounce mt-6 bg-[#52d591] text-[#fff] rounded-full px-28 py-6 hover:bg-[#f0ff00] border-4 border-black  font-comic  text-[24px] ">
         BUY $POPS
       </button></a>
 
